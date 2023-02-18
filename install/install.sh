534c4799fe01a23801d92305a7d4d70be5b07ca7
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2023-02-19 01:16:07 +0900

    mv install shels

--- install/install.sh
+++ install/install.sh
@@ -25,3 +25,42 @@ export DOTFILES="$HOME/dotfiles"
 # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # ここにAppleにログインしたか確認のメッセを出す
+
+has() {
+    type "$1" > /dev/null 2>&1
+}
+
+if [[ ! -d $DOTFILES ]]; then
+    if has "git"; then
+        git clone https://github.com/HidakaRintaro/dotfiles.git $DOTFILES
+    elif has "curl" || has "wget"; then
+        TARBALL="https://github.com/HidakaRintaro/dotfiles/archive/refs/heads/main.tar.gz"
+        if has "curl"; then
+            curl -L ${TARBALL} -o main.tar.gz
+        else
+            wget ${TARBALL}
+        fi
+        tar -zxvf main.tar.gz
+        rm -f main.tar.gz
+        mv -f dotfiles-main "$DOTFILES/dotfiles"
+    else
+        echo "curl or wget or git required"
+        exit 1
+    fi
+
+    cd ${DOT_DIR}
+    for f in *;
+    do
+        [[ "$f" == ".git" ]] && continue
+        [[ "$f" == ".gitignore" ]] && continue
+        [[ "$f" == ".DS_Store" ]] && continue
+        [[ "$f" == "README.md" ]] && continue
+        [[ "$f" == "install.sh" ]] && continue
+
+        ln -snf $DOT_DIR/"$f" $HOME/".$f"
+        echo "Installed .$f"
+    done
+else
+    echo "dotfiles already exists"
+    exit 1
+fi
