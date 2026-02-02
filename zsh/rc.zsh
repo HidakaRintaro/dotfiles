63883e41f356692401e7b82c2a9db079066a1cb0
Hidaka Rintaro (rinchan) <rintaro@rintaro.dev>
2026-02-02 23:46:36 +0900

    Merge pull request #3 from HidakaRintaro/update-2026/01
    update 2026-01

--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -4,15 +4,18 @@ source_if_exists () {
   fi
 }
 
+source_if_exists $HOME/dotfiles/zsh/homebrew.zsh
 source_if_exists $HOME/dotfiles/zsh/aliases.zsh
 source_if_exists $HOME/dotfiles/zsh/starship.zsh
 
+# Load Starship
+eval "$(starship init zsh)"
 
 autoload -Uz compinit && compinit  # 補完機能を有効にする
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'  # 小文字でも大文字ディレクトリ、ファイルを補完できるようにする
 
-# Added by Docker Desktop
-source /Users/hidakarintaro/.docker/init-zsh.sh || true 
+# Claude
+export PATH="$HOME/.claude/local:$PATH"
 
-# Load Starship
-eval "$(starship init zsh)"
+# pnpmの使用を許可
+export VOLTA_FEATURE_PNPM=1
