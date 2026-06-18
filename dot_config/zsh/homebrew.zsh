1fec40a6d79dbf6c86f5317247f65429daf0bcfa
日髙凜太郎 <hidakarintaro@HidakaRintaroMacBook.local>
2026-06-18 22:33:30 +0900

    Revert Homebrew path cleanup

--- dot_config/zsh/homebrew.zsh
+++ dot_config/zsh/homebrew.zsh
@@ -1,36 +1,16 @@
 # homebrewでインストールしたパッケージのpathを通す設定
 
-prepend_path_if_exists () {
-  if [[ -d "$1" ]]; then
-    export PATH="$1:$PATH"
-  fi
-}
+# coreutils (GNUのやつ)
+export PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH"
 
-if command -v brew >/dev/null 2>&1; then
-  HOMEBREW_PREFIX="$(brew --prefix)"
-elif [[ -x /opt/homebrew/bin/brew ]]; then
-  HOMEBREW_PREFIX="/opt/homebrew"
-elif [[ -x /usr/local/bin/brew ]]; then
-  HOMEBREW_PREFIX="/usr/local"
-fi
+# make (gmake)
+export PATH="/opt/homebrew/opt/make/libexec/gnubin:$PATH"
 
-if [[ -n "${HOMEBREW_PREFIX:-}" ]]; then
-  # coreutils (GNU)
-  prepend_path_if_exists "$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin"
+# sed (gsed)
+export PATH="/opt/homebrew/opt/gnu-sed/libexec/gnubin:$PATH"
 
-  # make (gmake)
-  prepend_path_if_exists "$HOMEBREW_PREFIX/opt/make/libexec/gnubin"
+# curl (gcurl)
+export PATH="/opt/homebrew/opt/curl/bin:$PATH"
 
-  # sed (gsed)
-  prepend_path_if_exists "$HOMEBREW_PREFIX/opt/gnu-sed/libexec/gnubin"
-
-  # curl (gcurl)
-  prepend_path_if_exists "$HOMEBREW_PREFIX/opt/curl/bin"
-
-  # grep (ggrep)
-  prepend_path_if_exists "$HOMEBREW_PREFIX/opt/grep/libexec/gnubin"
-
-  unset HOMEBREW_PREFIX
-fi
-
-unfunction prepend_path_if_exists
+# grep (ggrep)
+export PATH="/opt/homebrew/opt/grep/libexec/gnubin:$PATH"
